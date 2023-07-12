f174e4f2a53c216eec93282f472df2d3feb39d26
Wallmonger <nicolas.berrier360@gmail.com>
2023-07-12 21:58:43 +0200

    Movement done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,19 +5,42 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public CharacterController cc;
+
+    // Variables pour le déplacement
     public float moveSpeed;
     public float jumpForce;
     public float gravity;
+
+    // Vecteur directionnel souahaité
     private Vector3 moveDir;
 
     // GetAxis test directional arrow keys
     void Update()
-    {
+    {   // ###  Calcul de la direction ###
         // Nous enregistrons un nouveau vector3 pour les axes horizontaux et verticaux (x, y, z) ce qui permet d'avoir les différents axes par défaut (projetSettings/InputManager)
         // Pour l'axe y, nous ne touchons rien car le personnage ne peut pas voler
-        // cc.Move active notre direction, toujours multiplier par Time.deltaTime pour ne pas prendre en compte les performances du système
+        
         moveDir = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDir.y, Input.GetAxis("Vertical") * moveSpeed);
+
+        // ### Vérification de la touche espace depuis l'InputManager ###
+        if (Input.GetButtonDown("Jump"))
+        {
+            moveDir.y = jumpForce;
+        }
+
+        // ### Application de la gravité ###
         moveDir.y -= gravity * Time.deltaTime;
+
+
+        // ### Si mouvement != 0 alors on applique une rotation ###
+        if (moveDir.x != 0 || moveDir.z != 0)
+        {
+            // Slerp permet d'effectuer une rotation fluide (currentRotation, targetRotation, transitionSpeed) 
+            // Look rotation prend en paramètre un new Vector3 pour calculer la rotation souhaitée
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(new Vector3 (moveDir.x, 0, moveDir.z)), 0.5f);
+        }
+
+        // ### Déplacement ###
         cc.Move(moveDir * Time.deltaTime);
     }
 }
