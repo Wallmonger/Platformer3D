0035e54b6d0cf3911fd057c8677d517a2d4d9f4c
Wallmonger <nicolas.berrier360@gmail.com>
2023-07-12 21:08:56 +0200

    Camera fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public CharacterController cc;
+    public float moveSpeed;
+    public float jumpForce;
+    public float gravity;
+    private Vector3 moveDir;
 
-    // Update is called once per frame
+    // GetAxis test directional arrow keys
     void Update()
     {
-        
+        // Nous enregistrons un nouveau vector3 pour les axes horizontaux et verticaux (x, y, z) ce qui permet d'avoir les différents axes par défaut (projetSettings/InputManager)
+        // Pour l'axe y, nous ne touchons rien car le personnage ne peut pas voler
+        moveDir = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDir.y, Input.GetAxis("Vertical") * moveSpeed);
+        cc.Move(moveDir * Time.deltaTime);
     }
 }
