39527e73545f02c7e745feb94d5e805e07dc2648
Wallmonger <nicolas.berrier360@gmail.com>
2023-07-14 20:00:15 +0200

    Instanciation & particles

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -7,12 +7,19 @@ public class PlayerCollision : MonoBehaviour
 {
     
     public int nbCoins = 0;
+    public GameObject pickupEffect;
+    public GameObject mobEffect;
+
     // OnTriggerEnter detect all objects with the property "isTrigger(true)" in collision with the gameObject
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "coin")
         {
+            // Quand une pièce est rammassée, une instance de notre système de particule est appelée, à la position de la pièce touchée, et une rotation de base (Quaternion.identity)
+            // On enregistre le tout dans une variable de type GameObject pour pouvoir effectuer la suppression au cours du temps
+            GameObject go = Instantiate(pickupEffect, other.transform.position, Quaternion.identity);
+            Destroy(go, 0.5f);
             nbCoins++;
             Destroy(other.gameObject);
         }
@@ -25,12 +32,14 @@ public class PlayerCollision : MonoBehaviour
         if (collision.gameObject.tag == "hurt")
         {
             print("You are wounded");
+            
         }
 
         if (collision.gameObject.tag == "mob")
         {
             print("Damage To Mob : 5");
-
+            GameObject go = Instantiate(mobEffect, collision.transform.position, Quaternion.identity);
+            Destroy(go.gameObject, 0.6f);
             // Destroy the parent object and not only the back of the mob
             Destroy(collision.gameObject.transform.parent.gameObject);
         }
