e9e6bffad92dfb4fe574d8a88931d03013a83560
Wallmonger <nicolas.berrier360@gmail.com>
2023-07-16 22:16:12 +0200

    Feedbacks on contact

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -14,8 +14,20 @@ public class PlayerCollision : MonoBehaviour
     public GameObject cam1;
     public GameObject cam2;
     public GameObject cam3;
+    public AudioClip hitSound;
+    public AudioClip coinSound;
+    private AudioSource audioSource;
 
     // OnTriggerEnter detect all objects with the property "isTrigger(true)" in collision with the gameObject
+
+
+    private void Start()
+    {
+        // On instancie la variable audioSource pour la connecter à notre composant, et ainsi accéder à ses méthodes telles que playOneShot pour jouer nos sons.
+        audioSource = GetComponent<AudioSource>();
+    }
+
+
 
     private void OnTriggerEnter(Collider other)
     {
@@ -23,6 +35,7 @@ public class PlayerCollision : MonoBehaviour
         {
             // Quand une pièce est rammassée, une instance de notre système de particule est appelée, à la position de la pièce touchée, et une rotation de base (Quaternion.identity)
             // On enregistre le tout dans une variable de type GameObject pour pouvoir effectuer la suppression au cours du temps
+            audioSource.PlayOneShot(coinSound);
             GameObject go = Instantiate(pickupEffect, other.transform.position, Quaternion.identity);
             Destroy(go, 0.5f);
             nbCoins++;
@@ -72,18 +85,21 @@ public class PlayerCollision : MonoBehaviour
         if (collision.gameObject.tag == "hurt")
         {
             print("You are wounded");
+            iTween.PunchScale(gameObject, new Vector3(0.2f, 0.2f, 0.2f), .5f);
             
         }
 
         if (collision.gameObject.tag == "mob" && canInstantiate)
         {
             canInstantiate = false;
-            
+            audioSource.PlayOneShot(hitSound);
+            iTween.PunchPosition(gameObject, Vector3.back * 5, 0.5f);
+            /*iTween.PunchScale(collision.gameObject.transform.parent.gameObject, new Vector3(50,50,50), .6f);*/
             print("Damage To Mob : 5");
             GameObject go = Instantiate(mobEffect, collision.transform.position, Quaternion.identity);
-            Destroy(go.gameObject, 0.6f);
+            Destroy(go.gameObject, 0.5f);
             // Destroy the parent object and not only the back of the mob
-            Destroy(collision.gameObject.transform.parent.gameObject);
+            Destroy(collision.gameObject.transform.parent.gameObject, 0.6f);
 
             // StartCoroutine est la seule manière d'appeler une fonction de type IEnumerator
             StartCoroutine("ResetInstanciate");
